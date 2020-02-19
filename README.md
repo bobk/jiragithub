dc959d034e0ffc66a4b0f43d73b6a31de4adb109
bobk <ramentokyo@gmail.com>
2020-02-19 13:13:38 -0500

    JIRAGITHUB-2 update code for cleanliness, comments, documentation

--- README.md
+++ README.md
@@ -1,5 +1,45 @@
 # jiragithub
+
 This example code set uses the PyGitHub and jira-python libraries to perform various bits of useful interoperability between Jira and GitHub
 
-jiragithub-update_comments.py: based on Jira issuekeys found in GitHub commit messages, adds GitHub commit information as comments to the associated Jira issues
+### jiragithub-update_comments.py: 
+    
+- based on Jira issuekeys found in GitHub commit messages, adds GitHub commit information as comments to the associated Jira issues
+
+#### general notes
+
+1) intended for low-volume environments that just want a simple mechanism to update Jira issues with info on related GitHub commits
+1) for environments that are making large numbers of commits against small numbers of Jira issues, this code may not be the best answer
+1) does not require a Jira plug-in, custom fields or any changes requiring elevated Jira access
+1) requires:
+   * a GitHub personal access token (PAT) that includes at least 'repo:' scope (no writes are made to GitHub, only reads)
+   * Jira project(s) containing issues referenced in GitHub commits
+   * write access (e.g. Developer role, but not Administrator role) to those Jira projects
+1) areas for code enhancement:
+   * store commit info in a custom field
+   * add commit stats (files, lines)
+   * add code to remove older comments (comments added by this code)
+
+#### configuration file
+
+   * general
+      * jira_server - the Jira server URL to connect to
+      * jira_projects - list of projects to scan for in GitHub commit messages
+      * jira_comment_footprint - (for future use) key phrase in comments so we know that they were made by this code
+      * github_org - GitHub organization or user ID that all repos in github_repos section are under
+   * github_repos
+      * <repo_name>
+         * repo_lastscan_timestamp - timestamp of last run for this script for this repo (this is updated by the code itself)
+         * jira_comment_format - sample comment format, using <bracketed variable names>
+
+#### execution
+
+1) set the GITHUB_PASSWORD env var to your GitHub PAT
+1) set the JIRA_USERNAME and JIRA_PASSWORD env vars to the correct values 
+   * (note that for Jira Server, you will have to use a real username and password, for Jira Cloud, you can use a username and API token)
+1) run the script
+
+
+
+
 
--- jiragithub-update_comments.py
+++ jiragithub-update_comments.py
@@ -18,6 +18,8 @@ import requests
 from datetime import datetime
 from lxml import html
 
+#   this is an undocumented and unsupported GitHub HTML API for getting the branch(es) containing a commit
+#   since it is just HTML we have to parse it with lxml and xpath
 def get_branches_commit(repo_html_url, commit_sha):
 
     api_html_url = repo_html_url.strip("/") + "/branch_commits/" + commit_sha
@@ -30,99 +32,73 @@ def get_branches_commit(repo_html_url, commit_sha):
 
 def main():
 
-    githubkey = os.environ['GITHUB_PASSWORD']
-    githubconn = Github(githubkey)
-
+#   read in all of our config file values - see docs for explanation of each
     with open('jiragithub-config.json', 'r') as config_file:
         config = json.load(config_file)
+        config_file.close
+    jira_projects = config['general']['jira_projects']
+    jira_comment_footprint = config['general']['jira_comment_footprint']
 
-    config['general']['foo'] = "fff"
-    with open('jiragithub-config.json', 'w') as config_file:
-        json.dump(config, config_file, indent=8)
+#   get GitHub PAT from env var and connect to GitHub
+    githubkey = os.environ['GITHUB_PASSWORD']
+    github_base_url = config['general']['github_base_url']
+    if (github_base_url != ''):
+        githubconn = Github(login_or_token=githubkey, base_url=github_base_url)
+    else:
+        githubconn = Github(login_or_token=githubkey)
+    github_org = config['general']['github_org']
 
+#   connect to Jira also
     jira_server = config['general']['jira_server']
     jira_username = os.environ['JIRA_USERNAME']
     jira_password = os.environ['JIRA_PASSWORD']
     options = { "server" : jira_server }
     jira = JIRA(options, basic_auth=(jira_username, jira_password))
 
-    jira_projects = config['general']['jira_projects']
-
+#   go through each GitHub repo in the config file, look for commits since the last scan time with(for that repo)
+#   that have a commit message that contains a Jira issuekey that is in our project list
     for repo_name in config['github_repos']:
-        repo_url = config['github_repos'][repo_name]['repo_url']
-        repo_lastscan_time = datetime.fromisoformat(config['github_repos'][repo_name]['repo_lastscan_time'])
+        repo_lastscan_timestamp_text = config['github_repos'][repo_name]['repo_lastscan_timestamp']
+        if not (repo_lastscan_timestamp_text == ''):
+            repo_lastscan_timestamp = datetime.fromisoformat(repo_lastscan_timestamp_text)
+        else:
+            repo_lastscan_timestamp = datetime.min
         jira_comment_format = config['github_repos'][repo_name]['jira_comment_format']
-        jira_comment_maxnum = int(config['github_repos'][repo_name]['jira_comment_maxnum'])
 
-        repo = githubconn.get_repo('bobk/' + repo_name)
-        print(repo)
-        commits = repo.get_commits(since=repo_lastscan_time)
+#   connect to the repo and get all the commits since the last scan time
+        repo = githubconn.get_repo(github_org + '/' + repo_name)
+        commits = repo.get_commits(since=repo_lastscan_timestamp)
         for commit in commits:
-            #print(dir(commit))
-            #print(dir(commit.commit))
             message = commit.commit.message
+#   for each commit, look in all the projects to see if there is a match
             for jira_project in jira_projects:
                 pattern = re.compile(r'(' + jira_project + '-[0-9]+)')
                 match = pattern.match(message)
+#   if there is a match, write a new comment to the Jira issue
                 if (match):
                     jira_issue = jira.issue(match.group(1))
                     if (jira_issue):
+
+#   construct the new comment
                         jira_comment = jira_comment_format
                         jira_comment = jira_comment.replace('<repo_name>', repo_name)
                         jira_comment = jira_comment.replace('<repo_url>', repo.html_url)
                         branch_name = get_branches_commit(repo.html_url, commit.commit.sha)[0]
                         jira_comment = jira_comment.replace('<branch_name>', branch_name)
-                        branch_url = repo.html_url + "/tree" + branch_name
+                        branch_url = repo.html_url + "/tree/" + branch_name
                         jira_comment = jira_comment.replace('<branch_url>', branch_url)
                         jira_comment = jira_comment.replace('<commit_message>', commit.commit.message)
                         jira_comment = jira_comment.replace('<commit_url>', commit.commit.html_url)
                         jira_comment = jira_comment.replace('<commit_abbrev>', commit.commit.sha[:7])
                         jira_comment = jira_comment.replace('<commit_author>', commit.commit.author.name)
-                        #jira_comment = jira_comment.replace('<branch_name>', commit.)
-                        #print(jira_comment)
+
+#   add the new comment
                         jira.add_comment(jira_issue, jira_comment)
-                        #print(get_branches_commit(repo.html_url, commit.commit.sha))
 
-                        if (jira_comment_maxnum >= 1):
-                            jira_allcomments = jira.comments(jira_issue)
-                            jira_allcomments.sort(key=lambda r: datetime.fromisoformat(r.created[:-5]))
-                            print(jira_allcomments)
-                            jira_comment_num = 0
-                            for comment in jira_allcomments:
-                                comment.delete()
-                                jira_comment_num = jira_comment_num + 1
-                                
-
-
-            
-            # print("   " + str(commit.commit.author.date) + "  " + commit.sha + "  " + commit.commit.message)
-            # for comment in commit.get_comments():
-            #     a=1
-            #     print(comment)
-            # for file in commit.files:
-            #     print("      " + file.filename + "   " + str(commit.stats.total))
-            #     a=1
-
-
-
-
-
-
-    # for repo in githubconn.get_user().get_repos():
-    #     print(repo.name)
-    #     if (repo.name == "jiracharts"):
-    #         commits = repo.get_commits()
-    #         for commit in commits:
-    #             print("   " + str(commit.commit.author.date) + "  " + commit.sha)
-    #             for comment in commit.get_comments():
-    #                 a=1
-    #                 print(comment)
-    #             for file in commit.files:
-    #                 print("      " + file.filename + "   " + str(commit.stats.total))
-    #                 a=1
-
-    print(githubconn.rate_limiting)
-    print(datetime.fromtimestamp(githubconn.rate_limiting_resettime))
+#   update our config file with the new timestamp, so that we do not process the same commits again
+        config['github_repos'][repo_name]['repo_lastscan_timestamp'] = str(datetime.today())
+        with open('jiragithub-config.json', 'w') as config_file:
+            json.dump(config, config_file, indent=4)
 
 if __name__== "__main__" :
     main()
